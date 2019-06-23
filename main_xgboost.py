22a0e0cced56ec113ba929c094b4bcc69d394000
farismismar <faris_mismar@hotmail.com>
2019-06-22 19:12:52 -0500

    Add files via upload

--- main_xgboost.py
+++ main_xgboost.py
@@ -10,11 +10,12 @@ import random
 import os
 import numpy as np
 import pandas as pd
+import math
 
 import itertools
 import xgboost as xgb
 from sklearn.metrics import roc_auc_score, roc_curve, auc
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import confusion_matrix
 
 import matplotlib
@@ -31,16 +32,24 @@ learning_rate = 0.01
 max_users = 54481
 r_exploitation = 0.3
 
-r_training = 0.8
-
+# in Mbps
 rate_threshold_35 = 4.3
 rate_threshold_28 = 2.9
 
+# in ms
+gap_duration = 1
+frame_duration = 10
+
+# in Watts
 PTX_35 = 1 # in Watts for 3.5 GHz
 PTX_28 = 1 # in Watts for 28 GHz
 
-p_blockage = 0.5
-blockage_loss = 40 # in dB
+# TODO:  Agree on these two values
+#p_blockage = 0.5 
+#p_blind_ho_succ = 0.5
+
+# TODO speed:
+v_s = 5 # km/h
 
 delta_f_35 = 180e3 # Hz/subcarrier
 delta_f_28 = 180e3 # Hz/subcarrier
@@ -54,6 +63,8 @@ Nf = 7 # dB noise fig.
 k_B = 1.38e-23 # Boltzmann
 T = 290 # Kelvins
 
+N_exploit = int(r_exploitation * max_users)
+
 # 1) Read the data
 # Add a few lines to caputre the seed for reproducibility.
 random.seed(seed)
@@ -62,7 +73,7 @@ np.random.seed(seed)
 def create_dataset():
     # Takes the two .csv files and merges them in a way that is useful for the Deep Learning.
     df35 = pd.read_csv('dataset/dataset_3.5_GHz.csv')
-    df28 = pd.read_csv('dataset/dataset_28_GHz.csv')
+    df28 = pd.read_csv('dataset/dataset_28_GHz_blockage.csv')
     
     # Truncate to the first max_users rows, for efficiency for now
     df35 = df35.iloc[:max_users,:]
@@ -127,9 +138,9 @@ def create_dataset():
     channel_gain_28 = np.array(channel_gain_28).astype(float)
     channel_gain_35 = np.array(channel_gain_35).astype(float)
     
-    # introduce blockage as Bernoulli rando variable
-    p = np.randon.binomial(1, p=p_blockage)
-    channel_gain_28 *= 10 ** (p * blockage_loss/10.)
+    # introduce blockage as Bernoulli random variable
+    #p = np.randon.binomial(1, p=p_blockage)
+    #channel_gain_28 *= 10 ** (p * blockage_loss/10.)
         
     noise_floor_35 = k_B * T * delta_f_35
     noise_floor_28 = k_B * T * delta_f_28
@@ -189,16 +200,17 @@ def plot_confusion_matrix(y_test, y_pred, y_score):
     
     plt.tight_layout()
     plt.savefig('figures/conf_matrix.pdf', format='pdf')
-    
-    # Compute area under ROC curve
-    roc_auc = roc_auc_score(y_test, y_score[:,1])
-    print('The ROC AUC for this UE is {0:.6f}'.format(roc_auc))
 
-def plot_roc(y_test, y_score, i=0):
-    # Compute ROC curve and ROC area
+def generate_roc(y_test, y_score):
     fpr, tpr, _ = roc_curve(y_test, y_score)
     roc_auc = auc(fpr, tpr)
-    
+
+    roc_auc_score_value = roc_auc_score(y_test, y_score)
+    print('The ROC AUC for this UE is {0:.6f}'.format(roc_auc_score_value))
+
+    return fpr, tpr, roc_auc, roc_auc_score 
+
+def plot_roc(fpr, tpr, roc_auc, i=0):
     plt.figure(figsize=(8,5))
     
     plt.rc('text', usetex=True)
@@ -225,13 +237,77 @@ def plot_roc(y_test, y_score, i=0):
 #    plt.title(r'\textbf{Receiver Operating Characteristic -- UE \#' + '{0}'.format(i) + '}')
     plt.legend(loc="lower right")
     plt.savefig('figures/roc_{0}.pdf'.format(i), format='pdf')
+    
+def plot_throughput_cdf(T):
+    fig = plt.figure(figsize=(10.24, 7.68))
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    matplotlib.rcParams['text.usetex'] = True
+    matplotlib.rcParams['font.size'] = 20
+    matplotlib.rcParams['text.latex.preamble'] = [
+        r'\usepackage{amsmath}',
+        r'\usepackage{amssymb}']   
+    
+    labels = T.columns
 
-def train_classifier(df):
-    dataset = df.copy()    
-    N_training = int(r_training * dataset.shape[0])
+    num_bins =  50
+
+    for data in T:
+        data_ = T[data]
+
+        counts, bin_edges = np.histogram(data_, bins=num_bins, density=True)
+        cdf = np.cumsum(counts) / counts.sum()
         
-    training = dataset.iloc[:N_training,:]
-    test = dataset.iloc[N_training:,:]
+        ax = fig.gca()
+        ax.plot(bin_edges[1:], cdf)
+    
+    plt.legend(labels, loc="upper left")    
+    plt.grid()
+    plt.xlabel('Throughput (Mbps)')
+    plt.ylabel('Throughput CDF')    
+    plt.savefig('figures/throughputs.pdf', format='pdf')
+    
+def plot_primary(X,Y, title, xlabel, ylabel, filename='plot.pdf'):
+    fig = plt.figure(figsize=(10.24,7.68))
+
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    matplotlib.rcParams['text.usetex'] = True
+    matplotlib.rcParams['font.size'] = 20
+    matplotlib.rcParams['text.latex.preamble'] = [
+        r'\usepackage{amsmath}',
+        r'\usepackage{amssymb}']
+    
+    #plt.title(title)
+    plt.xlabel(xlabel)
+    
+    ax = fig.gca()
+ #   ax.xaxis.set_major_locator(MultipleLocator(1))
+    # Format the ticklabel to be 2 raised to the power of `x`
+#    ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: int(2**x)))
+    
+    ax.set_autoscaley_on(True)
+    
+    plot_, = ax.plot(X, Y, 'k^-') #, label='ROC')
+
+#    ax.set_xlim(xmin=0.15, xmax=0.55)
+    
+    ax.set_ylabel(ylabel)
+#    ax.set_ylim(0.99, 1.01)
+    
+    plt.grid(True)
+    fig.tight_layout()
+    plt.savefig('figures/{}'.format(filename), format='pdf')
+    plt.show()
+
+def train_classifier(df, r_training=0.8):
+    dataset = df.copy()
+    
+#    N_training = int(r_training * dataset.shape[0])
+#    training = dataset.iloc[:N_training,:]
+#    test = dataset.iloc[N_training:,:]
+    
+    training, test = train_test_split(dataset, train_size=r_training, random_state=seed)
     
     eps = 1e-9
     X_train = training.drop('y', axis=1)
@@ -297,6 +373,29 @@ def predict_handover(df, clf):
        
     return y_pred
 
+def get_coherence_time(df, freq):
+    c = 299792458 # m/s
+    BS_x, BS_y, BS_z = [235.504198, 489.503816, 6]
+
+    # Check if freq is mmWave 
+    # then the beam coherence time
+    # else 
+    # OFDM coherence
+    # Constant for all users
+    
+    # Obtain D
+    # alpha AoA equivalent random(0, pi) or 30 to 150 degrees
+    if (freq > 20e9): # mm-Wave
+        D = ((df['lon'] - BS_x) ** 2 + (df['lat'] - BS_y) ** 2 + (df['height'] - BS_z) ** 2) ** 0.5
+        Theta_n = 112 / 32. # 32 antennas in the aziumuth direction
+        alpha = np.random.uniform(0, math.pi, size=df.shape[0])
+        T_B = D / (v_s * 1e3 * np.sin(alpha)) * Theta_n / 2. # in ms.
+        
+        return np.array(T_B).mean()
+    
+    if (freq < 20e9): # sub-6
+        return c / (freq * v_s * 1e3) * 1e3 # in ms
+
 #create_dataset() # only uncomment for the first run.
 df_ = pd.read_csv('dataset.csv')
 
@@ -317,32 +416,109 @@ df = df[['Capacity_35', 'Capacity_28', 'lon', 'lat', 'height']]
 
 # َQ: Is there a correlation between the channels?
 
+# ----------------------------------------------------------------------------
 # Problem I am in 3.5 GHz (src) and want to HO to 28 GHz (target)
-# Assume the measurement gap is 1 second, therefore, try to predict the future 28[t + 1]
+# ----------------------------------------------------------------------------
 
+# Assume the measurement gap is 1 second, therefore, try to predict the future 28[t + 1]
 # Create a negative lag and difference signals
-df.loc[:,'Capacity_28_t+1'] = df.loc[:,'Capacity_28'].shift(-1)
-df.loc[:,'Capacity_28_dt+1'] = df.loc[:,'Capacity_28'].diff(-1)
+#df.loc[:,'Capacity_28_t+1'] = df.loc[:,'Capacity_28'].shift(-1)
+#df.loc[:,'Capacity_28_dt+1'] = df.loc[:,'Capacity_28'].diff(-1)
 
 # Drop the NA data (a single unwanted row)
 df.dropna(inplace=True)
 
 # Define the HO criterion here.
-df['y'] = pd.DataFrame((df.loc[:,'Capacity_35'] <= rate_threshold_35) & (df.loc[:,'Capacity_28_t+1'] >= rate_threshold_28), dtype=int)
+df['y'] = pd.DataFrame((df.loc[:,'Capacity_35'] <= rate_threshold_35) & (df.loc[:,'Capacity_28'] >= rate_threshold_28), dtype=int)
 
 # Change the order of columns to put 
-column_order = ['lon', 'lat', 'Capacity_35', 'Capacity_28', 'Capacity_28_t+1', 'Capacity_28_dt+1', 'y']
+#column_order = ['lon', 'lat', 'height', 'Capacity_35', 'Capacity_28', 'Capacity_28_t+1', 'Capacity_28_dt+1', 'y']
+column_order = ['lon', 'lat', 'height', 'Capacity_35', 'Capacity_28', 'y']
 df = df[column_order]
 
+##############################################################################
+# 1) Optimal algorithm
+##############################################################################
+df_optimal = df.copy()
+df_optimal.loc[:,'Capacity_Optimal'] = df_optimal[['Capacity_35', 'Capacity_28']].apply(np.max, axis=1)
+benchmark_data_optimal = df_optimal.iloc[N_exploit:,:]
+del df_optimal
+
+##############################################################################
+# 2) Legacy algorithm
+##############################################################################
+df_legacy = df.copy()
+df_legacy['y'] = pd.DataFrame((df_legacy.loc[:,'Capacity_35'] <= rate_threshold_35) & (df_legacy.loc[:,'Capacity_28'] >= rate_threshold_28), dtype=int)
+
+p_0 = gap_duration / frame_duration
+
+tx_f1 = 1 - gap_duration / get_coherence_time(df_legacy, freq=3.5e9) # 3.5
+tx_f2 = 1 / get_coherence_time(df_legacy, freq=28e9) # there is no measurement gap to the target frequency 28
+
+agg_rate = tx_f1 / (tx_f1 + tx_f2) * df_legacy.loc[:,'Capacity_35'] + tx_f2 / (tx_f1 + tx_f2) * df_legacy.loc[:,'Capacity_28']
+df_legacy.loc[:,'Capacity_Legacy'] = p_0 * 0 + (1 - p_0) * agg_rate
+
+benchmark_data_legacy = df_legacy.iloc[N_exploit:,:]
+del df_legacy
+
+##############################################################################
+# 3) Blind handover algorithm
+##############################################################################
+df_blind = df.copy()
+#df_blind['blockage'] = pd.DataFrame(np.random.binomial(1, p_blockage, size=df_blind.shape[0]))
+#df_blind['blind_ho_success'] = pd.DataFrame(np.random.binomial(1, p_blind_ho_succ, size=df_blind.shape[0])) # assume it succeeds only 50% of the time.
+
+df_blind['y'] = pd.DataFrame((df_blind.loc[:,'Capacity_35'] <= rate_threshold_35), dtype=int)
+df_blind.loc[df_blind['y'] == 1, 'Capacity_Blind'] = df_blind['Capacity_28']
+df_blind.loc[df_blind['y'] == 0, 'Capacity_Blind'] = df_blind['Capacity_35']
+
+## if there is no blockage, blind handover will not happen.  Stay in 3.5
+#df_blind.loc[:,'Capacity_Blind'] = df_blind.loc[df_blind['blockage'] == 0, 'Capacity_35']
+#
+## if there is blockage, blind handover will happen with a chance of success of 50%.  
+##      The throughput is 0 if the handover fails
+#df_blind.loc[(df_blind['blockage'] == 1) & (df_blind['blind_ho_success'] == 0), 'Capacity_Blind'] = 0
+##      The throughput is of the 28 GHz if the handover succeeds
+#df_blind.loc[(df_blind['blockage'] == 1) & (df_blind['blind_ho_success'] == 1), 'Capacity_Blind'] = df_blind['Capacity_28']
+
+benchmark_data_blind = df_blind.iloc[N_exploit:,:]
+del df_blind
+
+##############################################################################
+# 4) Proposed algorithm
+##############################################################################
+
 # Use this for the exploitation
-N_exploit = int(r_exploitation * max_users)
 train = df.iloc[:N_exploit,:]
 
-[y_pred, y_score, clf] = train_classifier(train)
+benchmark_data_proposed = df.iloc[N_exploit:,:]
 
-benchmark_data = df.iloc[N_exploit:,:]
-y_pred_proposed = predict_handover(benchmark_data, clf)
-y_score_proposed = clf.predict_proba(benchmark_data.drop(['y'], axis=1))
-y_test_proposed = benchmark_data['y']
+roc_graphs = pd.DataFrame()
+roc_auc_values = []
+# Change r_training and save roc1 then repeat
+X = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
+for r_t in X:
+    [y_pred, y_score, clf] = train_classifier(train, r_t)
+    y_pred_proposed = predict_handover(benchmark_data_proposed, clf)
+    y_score_proposed = clf.predict_proba(benchmark_data_proposed.drop(['y'], axis=1))
+    y_test_proposed = benchmark_data_proposed['y']
 
+    fpr, tpr, score, _ = generate_roc(y_test_proposed, y_score_proposed[:,1])
+    roc_auc_values.append(score)
+    
+    roc_graphs = pd.concat([roc_graphs, pd.DataFrame(fpr), pd.DataFrame(tpr)], axis=1)
+
+roc_graphs.to_csv('roc_output.csv', index=False)
+plot_primary(X, roc_auc_values, 'ROC vs Training', r'$r_\text{training}$', 'ROC AUC', filename='roc_vs_training.pdf')
+
+# Run these lines for the target r_train only.
 plot_confusion_matrix(y_test_proposed, y_pred_proposed, y_score_proposed)
+benchmark_data_proposed.loc[benchmark_data_proposed['y'] == 0, 'Capacity_Proposed'] = benchmark_data_proposed['Capacity_35']
+benchmark_data_proposed.loc[benchmark_data_proposed['y'] == 1, 'Capacity_Proposed'] = benchmark_data_proposed['Capacity_28']
+
+##############################################################################
+# Plotting
+##############################################################################
+data = pd.concat([benchmark_data_optimal['Capacity_Optimal'], benchmark_data_proposed['Capacity_Proposed'], benchmark_data_legacy['Capacity_Legacy'], benchmark_data_blind['Capacity_Blind'], benchmark_data_proposed['Capacity_35'], benchmark_data_proposed['Capacity_28']], axis=1)
+data.columns = ['Optimal', 'Proposed', 'Legacy', 'Blind', 'Sub-6 only', 'mmWave only']
+plot_throughput_cdf(data)
